f0319a224dbf3d9fe7833335b4a767621ba95506
sophy1 <jihye.sophy.won@gmail.com>
2018-01-29 15:48:01 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@
 
 function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     var arr = [1, 2, 3];
-    if (x >= 0 ) {
+    if (x < 0 ) {
         arr[x] = 3;
         arr[x-1] = 3;
     }
@@ -11,6 +11,14 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
 
 
 // This example code is not related with CWE.
+var test = "test"; // UNUSED_VAR_ASSIGN (multiple cause points)
+if (1) {
+  test = "1";
+} else {
+  test = "0";
+}
+
+
 function foo() {
     var i = "test";
     for (i = 0; i < 1; i++) {
