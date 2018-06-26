2b3c031bd63f6197a45dd6804bf6b043147d34ca
sophy1 <jihye.sophy.won@gmail.com>
2018-06-26 14:38:50 +0900

    Update test1.ts

--- js/test1.ts
+++ js/test1.ts
@@ -3,9 +3,14 @@ let b: number = 17;
  
 function showProduct(first: number, second: number): void {
     console.log("The product: " +  first * second);
+    if (first > second) {
+      return first;
+    } if (first < second) {
+       return second;
+    }
 }
 
 b = 'test';
 a = 'test1';
- 
+
 showProduct(a, b);
