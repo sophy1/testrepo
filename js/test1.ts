e906174d926f7c78c4739ee6aef7337a27a195e4
sophy1 <jihye.sophy.won@gmail.com>
2018-06-26 14:00:34 +0900

    Create test1.ts

---
+++ js/test1.ts
@@ -0,0 +1,10 @@
+let a: number = 12;
+let b: number = 17;
+ 
+function showProduct(first: number, second: number): void {
+    console.log("The product: " +  first * second);
+}
+
+b = 'test';
+ 
+showProduct(a, b);
