46ac1f16c29cef3cd09c4a284f9a8e5d6872ebca
sophy1 <jihye.sophy.won@gmail.com>
2018-12-21 13:21:41 +0900

    Update test2.js

--- js/test2.js
+++ js/test2.js
@@ -55,16 +55,3 @@ class B extends A {
 }
 
 new B();
-
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
-}
-function CWE_476() { // NULL_POINTER
-    var obj;
-    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
-    console.log(y);
-}
