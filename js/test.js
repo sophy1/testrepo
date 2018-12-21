1f85fae9d9c9a41c9ede70448dba49534fe5097c
sophy1 <jihye.sophy.won@gmail.com>
2018-12-21 13:22:20 +0900

    Update test.js

--- js/test.js
+++ js/test.js
@@ -40,3 +40,16 @@ function poo(headers) {
         }
     }
 }
+
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
+    }
+}
+function CWE_476() { // NULL_POINTER
+    var obj;
+    var y = obj.x;  // UNINITIALIZED_LOCAL_VAR
+    console.log(y);
+}
