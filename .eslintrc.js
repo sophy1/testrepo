5d00e61e477761ce211325414e341e8809f50207
sophy1 <jihye.sophy.won@gmail.com>
2023-03-30 15:43:17 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   //parser: "@babel/eslint-parser",
   parser: "babel-eslint",
-  extends: ["plugin:prettier/recommended", "airbnb"],
+  extends: ["plugin:prettier/recommended", "airbnb", "plugin:sonarjs/recommended"],
   parserOptions: {
     requireConfigFile: false,
     babelOptions: {
